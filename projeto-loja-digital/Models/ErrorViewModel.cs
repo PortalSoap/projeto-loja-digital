3036334ff5e859412d393eb72323363abdad7cd2
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2022-07-07 02:19:58 -0300

    Projeto iniciado

---
+++ projeto-loja-digital/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projeto_loja_digital.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
