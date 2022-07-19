07dbb2d9497519c3603cfc5cdc4543cb50ccce8a
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2022-07-19 02:34:55 -0300

    Classes Category e Meal criadas

---
+++ projeto-loja-digital/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace projeto_loja_digital.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Meal> Meals { get; set; }
+    }
+}
